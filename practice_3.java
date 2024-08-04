9bb6050dbb05442e0e90c14b0ece073acc0bc004
Sivasakthi <119642707+roobikaram@users.noreply.github.com>
2024-08-04 22:53:39 +0530

    Update practice_3

--- practice_3.java
+++ practice_3.java
@@ -9,6 +9,8 @@ public class firstprograme {
 	public static void main(String[] args) {
 		System.out.println(evenlyDivides(2996));
 	}
+	/* n is some value then the function calls there is a is consider as n and count is also initialized the 0
+	and here is while loop strats */
 	static int evenlyDivides (int n) {
 		int a = n;
 		int count = 0;
