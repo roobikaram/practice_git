e943f74231d51d95d36d0eecc3d8d1f6a8f92ef2
roobikaram <sivaaram16@gmail.com>
2024-08-04 22:42:06 +0530

    modified practice_3

--- practice_3.java
+++ practice_3.java
@@ -7,8 +7,14 @@ import java.math.*;
 public class firstprograme {
 
 	public static void main(String[] args) {
-		System.out.println(evenlyDivides(2));
+		System.out.println(evenlyDivides(2445));
 	}
+    /* 
+    example :
+        the n is 2445 and the n is consider as a initialized count as 0.
+        now a is entering into the while loop and now a = 2445/10 = 244 and count is 1 increased 
+        when while loop condition is false until while loop is run.
+    */
 	static int evenlyDivides (int n) {
 		int a = n;
 		int count = 0;
